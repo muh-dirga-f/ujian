cff6ab5f3e6cb2f8c1adabe1a7a8b50d80f928ac
muh-dirga-f <muh.dirga.f@gmail.com>
2024-09-28 02:34:44 +0800

    feat: Tambah CRUD pada Admin (Users dan Sekolah), Guru (kelas, mapel dan ujian) dan Siswa (Informasi kelas siswa)

--- app.js
+++ app.js
@@ -115,6 +115,232 @@ app.get('/admin/dashboard', (req, res) => {
   res.render('admin/dashboard', { user: req.session.user });
 });
 
+// Rute untuk manajemen pengguna
+app.get('/admin/users', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  db.all('SELECT * FROM guru UNION SELECT * FROM siswa UNION SELECT * FROM admin', [], (err, rows) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server error');
+    }
+    res.render('admin/users', { users: rows, user: req.session.user });
+  });
+});
+
+app.get('/admin/users/add', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  res.render('admin/add_user', { user: req.session.user });
+});
+
+app.post('/admin/users/add', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  const { fullname, username, password, userType, id_sekolah, nip, nis } = req.body;
+  let query, params;
+
+  switch (userType) {
+    case 'guru':
+      query = 'INSERT INTO guru (fullname, username, password, id_sekolah, nip) VALUES (?, ?, ?, ?, ?)';
+      params = [fullname, username, password, id_sekolah, nip];
+      break;
+    case 'siswa':
+      query = 'INSERT INTO siswa (fullname, nis, password, id_sekolah) VALUES (?, ?, ?, ?)';
+      params = [fullname, nis, password, id_sekolah];
+      break;
+    case 'admin':
+      query = 'INSERT INTO admin (fullname, username, password) VALUES (?, ?, ?)';
+      params = [fullname, username, password];
+      break;
+    default:
+      return res.status(400).send('Invalid user type');
+  }
+
+  db.run(query, params, function(err) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server error');
+    }
+    res.redirect('/admin/users');
+  });
+});
+
+app.get('/admin/users/edit/:id/:type', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  const { id, type } = req.params;
+  let query;
+  switch (type) {
+    case 'guru':
+      query = 'SELECT * FROM guru WHERE id_guru = ?';
+      break;
+    case 'siswa':
+      query = 'SELECT * FROM siswa WHERE id_siswa = ?';
+      break;
+    case 'admin':
+      query = 'SELECT * FROM admin WHERE id_admin = ?';
+      break;
+    default:
+      return res.status(400).send('Invalid user type');
+  }
+  db.get(query, [id], (err, row) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server error');
+    }
+    res.render('admin/edit_user', { user: req.session.user, editUser: row, userType: type });
+  });
+});
+
+app.post('/admin/users/edit/:id/:type', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  const { id, type } = req.params;
+  const { fullname, username, password, id_sekolah, nip, nis } = req.body;
+  let query, params;
+
+  switch (type) {
+    case 'guru':
+      query = 'UPDATE guru SET fullname = ?, username = ?, password = ?, id_sekolah = ?, nip = ? WHERE id_guru = ?';
+      params = [fullname, username, password, id_sekolah, nip, id];
+      break;
+    case 'siswa':
+      query = 'UPDATE siswa SET fullname = ?, nis = ?, password = ?, id_sekolah = ? WHERE id_siswa = ?';
+      params = [fullname, nis, password, id_sekolah, id];
+      break;
+    case 'admin':
+      query = 'UPDATE admin SET fullname = ?, username = ?, password = ? WHERE id_admin = ?';
+      params = [fullname, username, password, id];
+      break;
+    default:
+      return res.status(400).send('Invalid user type');
+  }
+
+  db.run(query, params, function(err) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server error');
+    }
+    res.redirect('/admin/users');
+  });
+});
+
+app.get('/admin/users/delete/:id/:type', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  const { id, type } = req.params;
+  let query;
+  switch (type) {
+    case 'guru':
+      query = 'DELETE FROM guru WHERE id_guru = ?';
+      break;
+    case 'siswa':
+      query = 'DELETE FROM siswa WHERE id_siswa = ?';
+      break;
+    case 'admin':
+      query = 'DELETE FROM admin WHERE id_admin = ?';
+      break;
+    default:
+      return res.status(400).send('Invalid user type');
+  }
+  db.run(query, [id], function(err) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server error');
+    }
+    res.redirect('/admin/users');
+  });
+});
+
+// Rute untuk manajemen sekolah
+app.get('/admin/schools', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  db.all('SELECT * FROM sekolah', [], (err, rows) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server error');
+    }
+    res.render('admin/schools', { schools: rows, user: req.session.user });
+  });
+});
+
+app.get('/admin/schools/add', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  res.render('admin/add_school', { user: req.session.user });
+});
+
+app.post('/admin/schools/add', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  const { npsn, nama_sekolah, alamat, kab_kota, provinsi } = req.body;
+  db.run('INSERT INTO sekolah (npsn, nama_sekolah, alamat, kab_kota, provinsi) VALUES (?, ?, ?, ?, ?)',
+    [npsn, nama_sekolah, alamat, kab_kota, provinsi],
+    function(err) {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Server error');
+      }
+      res.redirect('/admin/schools');
+    });
+});
+
+app.get('/admin/schools/edit/:id', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  const { id } = req.params;
+  db.get('SELECT * FROM sekolah WHERE id_sekolah = ?', [id], (err, row) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server error');
+    }
+    res.render('admin/edit_school', { user: req.session.user, school: row });
+  });
+});
+
+app.post('/admin/schools/edit/:id', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  const { id } = req.params;
+  const { npsn, nama_sekolah, alamat, kab_kota, provinsi } = req.body;
+  db.run('UPDATE sekolah SET npsn = ?, nama_sekolah = ?, alamat = ?, kab_kota = ?, provinsi = ? WHERE id_sekolah = ?',
+    [npsn, nama_sekolah, alamat, kab_kota, provinsi, id],
+    function(err) {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Server error');
+      }
+      res.redirect('/admin/schools');
+    });
+});
+
+app.get('/admin/schools/delete/:id', (req, res) => {
+  if (!req.session.user || req.session.user.type !== 'admin') {
+    return res.redirect('/');
+  }
+  const { id } = req.params;
+  db.run('DELETE FROM sekolah WHERE id_sekolah = ?', [id], function(err) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server error');
+    }
+    res.redirect('/admin/schools');
+  });
+});
+
 app.get('/guru/dashboard', (req, res) => {
   if (!req.session.user || req.session.user.type !== 'guru') {
     return res.redirect('/');
